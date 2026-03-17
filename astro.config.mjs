af5b97fc78e938cbb48ba90be4f9e8f3c289aef9
Peter <peter@example.com>
2026-03-17 17:33:41 +0800

    fix: 添加 trailingSlash 配置解决目录索引 404 问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://longxiazhuli.com',
   compressHTML: true,
+  trailingSlash: 'always',
   build: {
     inlineStylesheets: 'auto'
   }
